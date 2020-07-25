aabf90273b1154eb404563ef7641e329266751cc
BarteKKmita <bartekkmitaa@gmail.com>
2020-07-25 17:25:31 +0200

    Add Trees and graphs examples

---
+++ TreesAndGraphs/src/main/java/com/learning/treesandgraphs/BinaryTree.java
@@ -0,0 +1,137 @@
+package com.learning.treesandgraphs;
+
+public class BinaryTree {
+
+    private Node root;
+
+    void addValue(int value){
+        root = addRecursively(root, value);
+    }
+
+    private Node addRecursively(Node root, int value){
+        if (root == null) {
+            return new Node(value);
+        } else if (root.nodeValue < value) {
+            root.left = addRecursively(root.left, value);
+        } else if (root.nodeValue > value) {
+            root.right = addRecursively(root.right, value);
+        }
+        return root;
+    }
+
+    public int getSize(){
+        int size = 0;
+        return checkSize(size, root);
+    }
+
+    private int checkSize(int size, Node node){
+        if (node != null) {
+            size += 1 +
+                    checkSize(size, node.left) +
+                    checkSize(size, node.right);
+        }
+        return size;
+    }
+
+    boolean containsValueDeepSearch(int val){
+        return deepFirstSearch(val, root);
+    }
+
+    boolean containsValueBreadthSearch(int val){
+        return breadthFirstSearch(root, val);
+
+    }
+
+    private boolean breadthFirstSearch(Node node, int val){
+        if (node == null) {
+            return false;
+        }
+        return node.nodeValue == val || breadthFirstSearch(node.left, val) || breadthFirstSearch(node.right, val);
+    }
+
+    private boolean deepFirstSearch(int val, Node node){
+        if (node == null) {
+            return false;
+        } else {
+            if (node.nodeValue == val) {
+                return true;
+            } else {
+                return deepFirstSearch(val, node.left) || deepFirstSearch(val, node.right);
+            }
+        }
+    }
+
+    void deleteNode(int value){
+        root = deleteNodeRecursively(value, root);
+    }
+
+    private Node deleteNodeRecursively(int value, Node node){
+        if (value == node.nodeValue) {
+            return removeFoundNode(node);
+        } else if (value > node.nodeValue && node.left != null) {
+            return deleteNodeRecursively(value, node.left);
+        } else if (node.right != null) {
+            return deleteNodeRecursively(value, node.right);
+        }
+        return node;
+    }
+
+    private Node removeFoundNode(Node node){
+        if (node.left == null && node.right == null) {
+            return null;
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else {
+                return node.left;
+            }
+        }
+    }
+
+    public void printNumbersInOrder(){
+        printNumbersInRightOrder(root);
+    }
+
+    private void printNumbersInRightOrder(Node node){
+        if (node != null) {
+            printNumbersInRightOrder(node.right);
+
+            System.out.print(node.nodeValue + " ");
+            printNumbersInRightOrder(node.left);
+        }
+    }
+
+    public void printNumbersInPreOrder(){
+        printNumbersInPreOrder(root);
+    }
+
+    private void printNumbersInPreOrder(Node node){
+        if (node != null) {
+            System.out.print(node.nodeValue + " ");
+            printNumbersInPreOrder(node.right);
+            printNumbersInPreOrder(node.left);
+        }
+    }
+
+    public void printNumbersInPostOrder(){
+        printNumbersInPostOrder(root);
+    }
+
+    private void printNumbersInPostOrder(Node node){
+        if(node!=null){
+            printNumbersInPostOrder(node.right);
+            printNumbersInPostOrder(node.left);
+            System.out.print(node.nodeValue+" ");
+        }
+    }
+
+    private class Node {
+        int nodeValue;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.nodeValue = value;
+        }
+    }
+}
---
+++ TreesAndGraphs/src/test/java/com/learning/treesandgraphs/BinaryTreeTest.java
@@ -0,0 +1,142 @@
+package com.learning.treesandgraphs;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinaryTreeTest {
+
+    @Test
+    public void shouldReturnTreeSize(){
+        //Given
+        var expectedSize = 7;
+        BinaryTree binaryTree = createBinaryTree();
+        //Then
+        assertEquals(expectedSize, binaryTree.getSize());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCheckingExistingValueUsingDeepSearch(){
+        //Given
+        var searchValue = 4;
+        BinaryTree binaryTree = createBinaryTree();
+        //When
+        boolean containsValue = binaryTree.containsValueDeepSearch(searchValue);
+        //Then
+        assertTrue(containsValue);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenValueNotExistsUsingDeepSearch(){
+        //Given
+        var searchValue = 100;
+        BinaryTree binaryTree = createBinaryTree();
+        //When
+        boolean containsValue = binaryTree.containsValueDeepSearch(searchValue);
+        //Then
+        assertFalse(containsValue);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCheckingExistingValueUsingBreadthSearch(){
+        //Given
+        var searchValue = 4;
+        BinaryTree binaryTree = createBinaryTree();
+        //When
+        boolean containsValue = binaryTree.containsValueBreadthSearch(searchValue);
+        //Then
+        assertTrue(containsValue);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenValueNotExistsUsingBreadthSearch(){
+        //Given
+        var searchValue = 100;
+        BinaryTree binaryTree = createBinaryTree();
+        //When
+        boolean containsValue = binaryTree.containsValueBreadthSearch(searchValue);
+        //Then
+        assertFalse(containsValue);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenDeletingExistingValue(){
+        //Given
+        var value = 4;
+        BinaryTree binaryTree = createBinaryTree();
+        //When
+        binaryTree.printNumbersInOrder();
+        binaryTree.deleteNode(value);
+        //Then
+        assertFalse(binaryTree.containsValueDeepSearch(value));
+    }
+
+    private BinaryTree createBinaryTree(){
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.addValue(6);
+        binaryTree.addValue(4);
+        binaryTree.addValue(8);
+        binaryTree.addValue(3);
+        binaryTree.addValue(5);
+        binaryTree.addValue(7);
+        binaryTree.addValue(9);
+        return binaryTree;
+    }
+
+    @Nested
+    class PrintNumbersTest {
+
+        private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        private final PrintStream originalOut = System.out;
+
+        @BeforeEach
+        public void setUp(){
+            System.setOut(new PrintStream(outContent));
+        }
+
+        @AfterEach
+        public void tearDown(){
+            System.setOut(originalOut);
+        }
+
+        @Test
+        public void shouldPrintNumbersInOrder(){
+            //Given
+            BinaryTree binaryTreeTest = new BinaryTreeTest().createBinaryTree();
+            var expectedPrintOrder = "3 4 5 6 7 8 9 ";
+            //When
+            binaryTreeTest.printNumbersInOrder();
+            //Then
+            assertEquals(expectedPrintOrder, outContent.toString());
+        }
+
+        @Test
+        public void shouldPrintInPreOrder(){
+            //Given
+            var expectedPrintOrder="6 4 3 5 8 7 9 ";
+            BinaryTree binaryTree = new BinaryTreeTest().createBinaryTree();
+            //When
+            binaryTree.printNumbersInPreOrder();
+            //Then
+            assertEquals(expectedPrintOrder, outContent.toString());
+        }
+
+        @Test
+        public void shouldPrintInPostOrder(){
+            //Given
+            var expectedPrintOrder="3 5 4 7 9 8 6 ";
+            BinaryTree binaryTree = new BinaryTreeTest().createBinaryTree();
+            //When
+            binaryTree.printNumbersInPostOrder();
+            //Then
+            assertEquals(expectedPrintOrder, outContent.toString());
+        }
+    }
+}
